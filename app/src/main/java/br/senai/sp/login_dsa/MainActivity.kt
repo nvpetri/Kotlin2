230fe7bb39181655d2400f7bc0ee958fe3103669
Nicolas Petri <123608034+nvpetri@users.noreply.github.com>
2024-02-08 09:46:56 -0300

    começando

--- app/src/main/java/br/senai/sp/login_dsa/MainActivity.kt
+++ app/src/main/java/br/senai/sp/login_dsa/MainActivity.kt
@@ -3,13 +3,21 @@ package br.senai.sp.login_dsa
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import br.senai.sp.login_dsa.ui.theme.LoginDSATheme
 
 class MainActivity : ComponentActivity() {
@@ -22,10 +30,35 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Teste()
                 }
             }
         }
+    }
+}
+
+@Composable
+fun Teste() {
+    Surface(
+        modifier = Modifier
+            .height(200.dp)
+            .fillMaxWidth(),
+        color = Color(0xFA8A1C1C)
+    ) {
+        Box (
+            modifier = Modifier
+                .background(color = Color(0xFF770808))
+        ){
+
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun TestePreview() {
+    LoginDSATheme {
+        Teste()
     }
 }
 
@@ -37,7 +70,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     LoginDSATheme {
